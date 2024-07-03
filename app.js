0afbaa909cb61100172a06f8849dd3ce4208db96
Iza <155007065+izafit@users.noreply.github.com>
2024-07-03 15:54:22 +0200

    app.js

--- app.js
+++ app.js
@@ -101,6 +101,31 @@ function clearScreen() {
   mathSign.innerHTML = "";
 }
 
+class DarkMode {
+  constructor() {
+    this.button = document.querySelector("#checkbox");
+    this.theme = localStorage.getItem("theme");
+    this.init();
+  }
+
+  init() {
+    if (this.theme) {
+      document.documentElement.setAttribute("data-theme", this.theme);
+      if (this.theme === "dark") this.button.checked = "true";
+    }
+    this.button.addEventListener("change", this.switchTheme);
+  }
+
+  switchTheme = (e) => {
+    let theme = e.target.checked ? "dark" : "light"; 
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  };
+}
+
+const darkMode = new DarkMode();
+
+
 operatorsButtons.forEach((button) => button.addEventListener("click", operate));
 
 equalsButton.addEventListener("click", showResult);
